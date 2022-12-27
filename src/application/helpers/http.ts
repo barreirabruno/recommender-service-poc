f1ca3f372765195afe0e58919933c5c242bd91ad
brunobarreira <brunobarreirasilva@gmail.com>
2022-12-27 19:43:29 -0300

    feat: add ServerError

--- src/application/helpers/http.ts
+++ src/application/helpers/http.ts
@@ -1,4 +1,11 @@
+import { ServerError } from '@/domain/models/errors/server-error'
+
 export type HttpResponse<T = any> = {
   statusCode: number
   data: T
 }
+
+export const serverError = (error: Error): HttpResponse<Error> => ({
+  statusCode: 500,
+  data: new ServerError(error)
+})
