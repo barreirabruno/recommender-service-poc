b1aadfe8925dd3ea72d8929d7dc91c5104b55052
brunobarreira <brunobarreirasilva@gmail.com>
2022-12-26 23:37:28 -0300

    refactor: move services to their own files

--- tests/data/services/recommender-service.spec.ts
+++ tests/data/services/recommender-service.spec.ts
@@ -1,48 +1,28 @@
-import { RecommenderSponsorInterface, RecommenderSponsorNamespace } from '@/domain/features/recommender-service'
-import { DocumentToVectorTransformer } from '@/data/services/document-to-vector-transformer'
+import { TfIdf } from 'natural'
+import Vector from 'vector-object'
 
-class SimilarityCalculator {
-  perform (vectorData: any): any {
-    return [
-      {
-        id: 'any_sponsor_idA',
-        score: 0.8795587357647578
-      },
-      {
-        id: 'any_sponsor_idB',
-        score: 0.7818452786316625
-      },
-      {
-        id: 'any_sponsor_idC',
-        score: 0.7609400405953574
-      }
-    ]
-  }
-}
-
-class RecommenderSponsorService implements RecommenderSponsorInterface {
-  constructor (private readonly documentToVectorTransformer: DocumentToVectorTransformer,
-    private readonly calculateSimilarity: SimilarityCalculator) {}
-
-  async perform (params: RecommenderSponsorNamespace.Input): Promise<RecommenderSponsorNamespace.Output> {
-    const docToVector = this.documentToVectorTransformer.perform({ processedDocument: [{ id: 'any_sponsor_id', content: 'any_sponsor_content' }] })
-    const similarityCalculator = this.calculateSimilarity.perform(docToVector)
-
-    return {
-      sponsor: {
-        ...params
-      },
-      ...similarityCalculator
-    }
-  }
-}
+import { DocumentToVectorTransformer } from '@/data/services/document-to-vector-transformer-service'
+import { RecommenderSponsorService } from '@/data/services/recommender-service'
+import { SimilarityCalculator } from '@/data/services/similarity-calculator-service'
 
 type SutTypes = {
   recommenderService: RecommenderSponsorService
 }
 
+interface VectorServiceInterface {
+  create: (object: object) => any
+}
+
+class VectorService implements VectorServiceInterface {
+  create (object: object): any {
+    return new Vector(object)
+  }
+}
+
 const makeSut = (): SutTypes => {
-  const documentToVector = new DocumentToVectorTransformer()
+  const vectorService = new VectorService()
+  const tfidfDependency = new TfIdf()
+  const documentToVector = new DocumentToVectorTransformer(tfidfDependency, vectorService)
   const similarityCalculator = new SimilarityCalculator()
   return {
     recommenderService: new RecommenderSponsorService(documentToVector, similarityCalculator)
@@ -50,7 +30,6 @@ const makeSut = (): SutTypes => {
 }
 
 describe('recommender-service', () => {
-  test('', async () => {})
   test('should return a list of sponsors based on a root sponsor', async () => {
     const { recommenderService } = makeSut()
     const result = await recommenderService.perform({ id: 'any_sponsor_id', trainedData: {} })
