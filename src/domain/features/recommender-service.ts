4a565584e4bb5e447e6a7a074b0247b677c704c8
brunobarreira <brunobarreirasilva@gmail.com>
2022-12-26 21:45:37 -0300

    feat: add RecommenderSponsorInterface

---
+++ src/domain/features/recommender-service.ts
@@ -0,0 +1,19 @@
+import { Sponsor } from '../models/sponsor'
+
+export namespace RecommenderSponsorNamespace {
+  export type Input = {
+    id: string
+    trainedData: any
+  }
+  export type Output = {
+    sponsor: {
+      id: string
+      content: string
+    }
+    similarSponsors: Sponsor[]
+  }
+}
+
+export interface RecommenderSponsorInterface {
+  perform: (params: RecommenderSponsorNamespace.Input) => Promise<RecommenderSponsorNamespace.Output>
+}
