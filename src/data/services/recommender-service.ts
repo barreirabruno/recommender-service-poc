0e165cd02158ce6638f8d93c5e718832c90a8b2e
brunobarreira <brunobarreirasilva@gmail.com>
2022-12-27 00:29:35 -0300

    refactor: rename SimilarityCalculator to SimilarityCalculatorService

--- src/data/services/recommender-service.ts
+++ src/data/services/recommender-service.ts
@@ -1,10 +1,10 @@
 import { RecommenderSponsorInterface, RecommenderSponsorNamespace } from '@/domain/features/recommender-service'
 import { DocumentToVectorTransformer } from './document-to-vector-transformer-service'
-import { SimilarityCalculator } from './similarity-calculator-service'
+import { SimilarityCalculatorService } from './similarity-calculator-service'
 
 export class RecommenderSponsorService implements RecommenderSponsorInterface {
   constructor (private readonly documentToVectorTransformer: DocumentToVectorTransformer,
-    private readonly calculateSimilarity: SimilarityCalculator) {}
+    private readonly calculateSimilarity: SimilarityCalculatorService) {}
 
   async perform (params: RecommenderSponsorNamespace.Input): Promise<RecommenderSponsorNamespace.Output> {
     const docToVector = this.documentToVectorTransformer.perform({ processedDocument: [{ id: 'any_sponsor_id', content: 'any_sponsor_content' }] })
--- tests/data/services/recommender-service.spec.ts
+++ tests/data/services/recommender-service.spec.ts
@@ -1,6 +1,6 @@
 import { DocumentToVectorTransformer } from '@/data/services/document-to-vector-transformer-service'
 import { RecommenderSponsorService } from '@/data/services/recommender-service'
-import { SimilarityCalculator } from '@/data/services/similarity-calculator-service'
+import { SimilarityCalculatorService } from '@/data/services/similarity-calculator-service'
 import { TfidCalculatorService } from '@/infra/natural/tfidf-calculator-service'
 import { VectorService } from '@/infra/vector-object/vector-service'
 
@@ -12,7 +12,7 @@ const makeSut = (): SutTypes => {
   const vectorService = new VectorService()
   const tfidfDependency = new TfidCalculatorService()
   const documentToVector = new DocumentToVectorTransformer(tfidfDependency, vectorService)
-  const similarityCalculator = new SimilarityCalculator()
+  const similarityCalculator = new SimilarityCalculatorService()
   return {
     recommenderService: new RecommenderSponsorService(documentToVector, similarityCalculator)
   }
