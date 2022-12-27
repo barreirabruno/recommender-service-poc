c450b77c7e76564c53e085c6a87be0991755ba5a
brunobarreira <brunobarreirasilva@gmail.com>
2022-12-26 23:29:13 -0300

    feat: implements SimilarityCalculator

---
+++ src/data/services/similarity-calculator-service.ts
@@ -0,0 +1,18 @@
+export class SimilarityCalculator {
+  perform (vectorData: any): any {
+    return [
+      {
+        id: 'any_sponsor_idA',
+        score: 0.8795587357647578
+      },
+      {
+        id: 'any_sponsor_idB',
+        score: 0.7818452786316625
+      },
+      {
+        id: 'any_sponsor_idC',
+        score: 0.7609400405953574
+      }
+    ]
+  }
+}
